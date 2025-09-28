e6d27700fb6ee7b664e73f609d29c19f7ec93676
tanishq-2004 <134104651+tanishq-2004@users.noreply.github.com>
2025-09-29 00:17:42 +0530

    1046-MAX-CONSECUTIVE-ONES-III Stats: Time: 81 ms (58.47%), Space: 20.3 MB (52.25%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
@@ -6,15 +6,12 @@ class Solution:
         n = len(nums)
         for r in range(n):
             if nums[r]==0:
-                num_zero += 1
-
-            while num_zero > k:
+                k -= 1
+            if k<0:
                 if nums[l]==0:
-                    num_zero -=1
+                    k += 1
                 l += 1
             w = r-l+1
-            max_num = max(max_num,w)
-        return max_num 
-
-
-        __import__("atexit").register(lambda: open("display_runtime.txt", "w").write("02"))
+            max_num = max(max_num,w)   
+        return max_num
+            
