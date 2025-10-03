035a289d9ce0d1acdadfba99c0b8a55915c26b6e
tanishq-2004 <134104651+tanishq-2004@users.noreply.github.com>
2025-10-03 23:59:52 +0530

    0283-MOVE-ZEROES Stats: Time: 8 ms (26.98%), Space: 18.8 MB (49.76%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,15 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        insert_pos = 0  # where the next non-zero should go
+        left = 0
 
-        # Step 1: Move all non-zeros to the front
-        for num in nums:
-            if num != 0:
-                nums[insert_pos] = num
-                insert_pos += 1
-
-        # Step 2: Fill the remaining positions with zeros
-        while insert_pos < len(nums):
-            nums[insert_pos] = 0
-            insert_pos += 1
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[left], nums[i] = nums[i], nums[left]
+                left += 1
